77333dec4420e52dc094575da20aeaf2cbb703ed
Vad Viktor <vad.viktor@gmail.com>
2011-10-03 14:14:47 +0200

    docblocks added, refactoring, preparing for recursive datamining

--- wmb.js
+++ wmb.js
@@ -1,25 +1,60 @@
 var ikon = {};
 ikon.wmb = {
-    debug_v : true, //visual alerts
-    debug_c : true, //console debug messages
+
+    /**
+     * Visual alerts
+     *
+     * @private
+     * @type boolean
+     */
+    debug_v : true,
+
+    /**
+     * Console debug messages
+     *
+     * @private
+     * @type boolean
+     */
+    debug_c : true,
+
+    /**
+     * Output div
+     *
+     * @private
+     * @type object
+     */
     output_element : null,
 
+    /**
+     * Collected tier reagents
+     *
+     * reagents [ tier ] [ item id ] {
+     *                                   name
+     *                                   icon
+     *                                   count
+     *                               }
+     * @private
+     * @type object
+     */
+    reagents : [],
+
+    /**
+     *
+     * @private
+     * @return object
+     */
     getAnchorElement : function()
     {
         var h2s = jqikon( "h2[class*='clear']" );
         var j_h2s = h2s.length;
-        for ( i = 0; i < j_h2s; i++ )
+        for ( var i = 0; i < j_h2s; i++ )
         {
             if ( jqikon( h2s[i] ).text() == 'Contribute' )
             {
                 return jqikon( h2s[i] ).parent();
             }
         }
-        this.errorNoAnchorElementFound();
-    },
-
-    errorNoAnchorElementFound : function()
-    {
+        //error
         var msg = "No element found as an output anchor";
         if ( this.debug_v )
         {
@@ -31,33 +66,112 @@ ikon.wmb = {
         }
     },
 
-    getOutputElement : function()
+    /**
+     *
+     * @public
+     * @return string
+     * @param url
+     */
+    getItemIdFromUrl : function( url )
+    {
+        var r = /item=[0-9]{1,}/;
+        var m = url.match( r );
+        var s = m[0].split( '=' );
+        return s[1];
+    },
+
+    /**
+     * Main entry point
+     *
+     * @public
+     * @param url string
+     */
+    disassemble : function( url )
+    {
+        //launch only once
+        if ( jqikon( '#ikon_wmb_output' ).length == 1 )
+        {
+            var msg = "Material breakdown already launched";
+            if ( this.debug_c )
+            {
+                console.warn( msg );
+            }
+            if ( this.debug_v )
+            {
+                alert( msg );
+            }
+            return;
+        }
+
+        //header
+        this.dumpOutput( '<div class="text">' +
+                         '<h2 id="ikon_wmb_header" class="clear">Material breakdown [processing]</h2>' +
+                         '<div class="block-bg"></div>' +
+                         '</div>' );
+
+        this.dumpItemReagentsRecursive( this.getItemIdFromUrl( url ), 0 );
+    },
+
+    /**
+     *
+     * @param item_id [string|integer]
+     * @param tier [string|integer]
+     */
+    dumpItemReagentsRecursive : function( item_id, tier )
+    {
+        var r = this.getItemReagents( item_id );
+    },
+
+    /**
+     *
+     * @param item_id [string|integer]
+     */
+    getItemReagents : function( item_id )
+    {
+        jqikon.ajax( {
+                         type : 'GET',
+                         url : 'http://www.wowhead.com/item=' + item_id + '&xml',
+                         dataType: 'xml',
+                         success: function( data, textStatus, jqXHR )
+                         {
+                             if ( ikon.wmb.debug_c )
+                             {
+                                 console.log( data );
+                                 console.log( textStatus );
+                                 console.log( jqXHR );
+                                 console.log( jqikon( data ).find( 'createdBy>spell reagent' ) );
+                             }
+                             if ( jqikon( data ).find( 'createdBy>spell reagent' ) )
+                             {
+
+                             }
+                         },
+                         error: function( jqXHR, textStatus, errorThrown )
+                         {
+                             if ( ikon.wmb.debug_c )
+                             {
+                                 console.error( jqXHR );
+                                 console.error( textStatus );
+                                 console.error( errorThrown );
+                             }
+                         }
+                     } );
+    },
+
+    /**
+     *
+     * @param html string
+     */
+    dumpOutput : function( html )
     {
         if ( this.output_element === null )
         {
-            jqikon('<div/>', {
+            jqikon( '<div/>', {
                 id: 'ikon_wmb_output'
-            }).insertBefore( this.getAnchorElement() );
+            } ).insertBefore( this.getAnchorElement() );
             this.output_element = jqikon( '#ikon_wmb_output' );
         }
 
-        return this.output_element;
-    },
-
-    //main entry point
-    disassemble : function( url )
-    {
-        this.dumpOutput( '<h1>wazeeeeeee</h1>' );
-    },
-
-
-//    getItemXml : function(item_id) {
-//        //return 'http//localhost/?url=http://www.wowhead.com/item=' + item_id + '&xml';
-//
-//    },
-
-    dumpOutput : function( html )
-    {
-        jqikon( this.getOutputElement() ).append( html );
+        jqikon( this.output_element ).append( html );
     }
 };
