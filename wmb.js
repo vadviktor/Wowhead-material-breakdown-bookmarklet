34f8251ba45e671ddd240e629fd278ff7c0eb6df
Vad Viktor <vad.viktor@gmail.com>
2011-10-03 18:38:14 +0200

    material gathering is getting into working condition

--- wmb.js
+++ wmb.js
@@ -31,7 +31,7 @@ ikon.wmb = {
      * reagents [ tier ] [ item id ] {
      *                                   name
      *                                   icon
-     *                                   count
+     *                                   number //integer always!
      *                               }
      * @private
      * @type object
@@ -47,9 +47,10 @@ ikon.wmb = {
     {
         var h2s = jqikon( "h2[class*='clear']" );
         var j_h2s = h2s.length;
-        for ( var i = 0; i < j_h2s; i++ )
+        var i = 0;
+        for ( i = 0; i < j_h2s; i++ )
         {
-            if ( jqikon( h2s[i] ).text() == 'Contribute' )
+            if ( jqikon( h2s[i] ).text() === 'Contribute' )
             {
                 return jqikon( h2s[i] ).parent();
             }
@@ -89,7 +90,7 @@ ikon.wmb = {
     disassemble : function( url )
     {
         //launch only once
-        if ( jqikon( '#ikon_wmb_output' ).length == 1 )
+        if ( jqikon( '#ikon_wmb_output' ).length === 1 )
         {
             var msg = "Material breakdown already launched";
             if ( this.debug_c )
@@ -105,57 +106,125 @@ ikon.wmb = {
 
         //header
         this.dumpOutput( '<div class="text">' +
-                         '<h2 id="ikon_wmb_header" class="clear">Material breakdown [processing]</h2>' +
+                         '<h2 id="ikon_wmb_header" class="clear">Material breakdown (processing)</h2>' +
                          '<div class="block-bg"></div>' +
                          '</div>' );
 
-        this.dumpItemReagentsRecursive( this.getItemIdFromUrl( url ), 0 );
+        this.gatherItemReagentsRecursive( this.getItemIdFromUrl( url ), 0 );
+
+        if ( this.debug_c )
+        {
+            console.log( this.reagents );
+        }
+
+
+
+        jqikon('#ikon_wmb_header').text('Material breakdown');
     },
 
     /**
      *
-     * @param item_id [string|integer]
-     * @param tier [string|integer]
+     * @param item_id string|integer
+     * @param tier string|integer
      */
-    dumpItemReagentsRecursive : function( item_id, tier )
+    gatherItemReagentsRecursive : function( item_id, tier )
     {
-        var r = this.getItemReagents( item_id );
+        tier = parseInt( tier );
+
+        var reagents = this.getItemReagents( item_id );
+        var reagents_count = reagents.length;
+        if ( reagents_count > 0 )
+        {
+            var i = 0;
+            for ( i = 0; i < reagents_count; i++ )
+            {
+                var r_id = this.addReagentToTier( reagents[i], tier );
+                this.gatherItemReagentsRecursive( r_id, tier + 1 );
+            }
+        }
+        else
+        {
+            if ( tier > 0 )
+            {
+                tier--;
+            }
+
+            this.reagents[tier][item_id].craftable = false;
+        }
     },
 
     /**
      *
-     * @param item_id [string|integer]
+     *
+     * @param reagent object
+     * @param tier string|integer
+     * @param craftable booelan [true]
+     * @return itemid integer|string
+     */
+    addReagentToTier : function( reagent, tier, craftable )
+    {
+        craftable = typeof craftable !== 'undefined' ? craftable : true;
+
+        var r_id = jqikon( reagent ).attr( 'id' );
+        var r_name = jqikon( reagent ).attr( 'name' );
+        var r_icon = jqikon( reagent ).attr( 'icon' );
+        var r_count = jqikon( reagent ).attr( 'count' );
+
+        if ( typeof this.reagents[tier] === 'undefined' )
+        {
+            this.reagents[tier] = [];
+        }
+
+        if ( typeof this.reagents[tier][r_id] === 'undefined' )
+        {
+            this.reagents[tier][r_id] = {
+                name : r_name,
+                icon : r_icon,
+                number : parseInt( r_count ),
+                craftable: craftable
+            };
+        }
+        else
+        {
+            this.reagents[tier][r_id].number += parseInt( r_count );
+        }
+
+        return r_id;
+    },
+
+    /**
+     *
+     * @param item_id string|integer
      */
     getItemReagents : function( item_id )
     {
-        jqikon.ajax( {
-                         type : 'GET',
-                         url : 'http://www.wowhead.com/item=' + item_id + '&xml',
-                         dataType: 'xml',
-                         success: function( data, textStatus, jqXHR )
-                         {
-                             if ( ikon.wmb.debug_c )
-                             {
-                                 console.log( data );
-                                 console.log( textStatus );
-                                 console.log( jqXHR );
-                                 console.log( jqikon( data ).find( 'createdBy>spell reagent' ) );
-                             }
-                             if ( jqikon( data ).find( 'createdBy>spell reagent' ) )
-                             {
-
-                             }
-                         },
-                         error: function( jqXHR, textStatus, errorThrown )
-                         {
-                             if ( ikon.wmb.debug_c )
-                             {
-                                 console.error( jqXHR );
-                                 console.error( textStatus );
-                                 console.error( errorThrown );
-                             }
-                         }
-                     } );
+        var xml_data = jqikon.ajax( {
+                                        type : 'GET',
+                                        async : false,
+                                        url : 'http://www.wowhead.com/item=' + item_id + '&xml',
+                                        dataType: 'xml',
+                                        success: function( data, textStatus, jqXHR )
+                                        {
+                                            if ( ikon.wmb.debug_c )
+                                            {
+                                                console.log( data );
+                                                console.log( textStatus );
+                                                console.log( jqXHR );
+                                                console.log( jqikon( data )
+                                                                 .find( 'createdBy>spell:first reagent' ) );
+                                            }
+                                        },
+                                        error: function( jqXHR, textStatus, errorThrown )
+                                        {
+                                            if ( ikon.wmb.debug_c )
+                                            {
+                                                console.error( jqXHR );
+                                                console.error( textStatus );
+                                                console.error( errorThrown );
+                                            }
+                                        }
+                                    } ).responseXML;
+        return jqikon( xml_data ).find( 'createdBy>spell:first reagent' );
     },
 
     /**
