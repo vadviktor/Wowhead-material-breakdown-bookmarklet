3ffbe50caa0c9df2277efd0d3fac84bfc284d8d7
Vad Viktor <vad.viktor@gmail.com>
2011-10-06 12:39:25 +0200

    homepage made ready, final url tunes ready, homepage fired and app ready to be beta tested

--- bookmarklet.js
+++ bookmarklet.js
@@ -10,15 +10,15 @@
 
     (function( W, D, C, $, V, H, S, s, d )
     {
-        if ( !($ = W.jqikon || W.jQuery) || (V = $.fn.jquery) < "ikon" || V > "Z" || C( $ ) )
+        if ( !($ = W.jqikon || W.jQuery) || (V = $.fn.jquery) < 'ikon' || V > 'Z' || C( $ ) )
         {
-            H = D.getElementsByTagName( "head" )[0];
-            S = D.createElement( "script" );
-            S.type = "text/javascript";
-            S.src = "http://code.jquery.com/jquery-latest.min.js";
+            H = D.getElementsByTagName( 'head' )[0];
+            S = D.createElement( 'script' );
+            S.type = 'text/javascript';
+            S.src = 'http://code.jquery.com/jquery-latest.min.js';
             S.onload = S.onreadystatechange = function()
             {
-                if ( !d && (!(s = this.readyState) || s == "loaded" || s == "complete") )
+                if ( !d && (!(s = this.readyState) || s == 'loaded' || s == 'complete') )
                 {
                     d = 1;
                     W.jqikon = $ = W.jQuery.noConflict( 1 );
